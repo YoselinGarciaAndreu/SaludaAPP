2ba112a222725752ecb7185e13b5fb8d99bfd969
Jose <mail@mail.org>
2023-10-31 13:10:12 +0100

    Version 1 de SaludaAPP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.pmdm.saludaapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.pmdm.saludaapp"
--- app/src/main/java/com/pmdm/saludaapp/MainActivity.kt
+++ app/src/main/java/com/pmdm/saludaapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.pmdm.saludaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnHello:AppCompatButton = findViewById(R.id.btnHello)
+        btnHello.setOnClickListener(){
+            Log.i("Test","pulsado")
+        }
+
+        }
     }
 }
