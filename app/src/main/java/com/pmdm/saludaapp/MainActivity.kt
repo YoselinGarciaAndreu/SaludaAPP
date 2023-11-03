ebfec5fce5efdeede4ded8585717a80c0f9e28ed
Jose <mail@mail.org>
2023-11-03 13:02:48 +0100

    Version casi final

--- app/src/main/java/com/pmdm/saludaapp/MainActivity.kt
+++ app/src/main/java/com/pmdm/saludaapp/MainActivity.kt
@@ -3,18 +3,20 @@ package com.pmdm.saludaapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import kotlin.math.log
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnHello:AppCompatButton = findViewById(R.id.btnHello)
+        val etName:AppCompatEditText = findViewById(R.id.etName)
         btnHello.setOnClickListener(){
-            Log.i("Test","pulsado")
+            //Log.i("Test","pulsado")
+            val name = etName.text.toString()
+            Toast.makeText(this, "Hola $name!", Toast.LENGTH_LONG).show()
         }
-
         }
     }
-}
